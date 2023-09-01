322d2b2235d643f5c00d614e2eb4705c5882eebc
malekabd <61131777+malekabd@users.noreply.github.com>
2023-09-01 22:56:22 +0300

    Pagnation, Aliasing,LimitimgFields,Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,23 +2,72 @@ const Tour = require('./../models/tourModel');
 /* const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );*/
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next()
+};
+
 exports.getAllTours = async (req, res) => {
   try {
+    //1A) Filtering
+    //console.log(req.query);
     //BUILD QUERY
     //how to take certain queries out
+    //console.log(req.query);
     const queryObj = { ...req.query };
+    //console.log(queryObj);
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
-    const query = Tour.find(queryObj);
+
+    //1B)Advanced filter
+
+    let queryStr = JSON.stringify(queryObj);
+    //console.log(queryStr)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    //console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2)Sorting
+    if (req.query.sort) {
+      //127.0.0.1:3000/api/v1/tours?sort=-price,ratingAverage
+      const sortBy = req.query.sort.split(',').join(' ');
+      // console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3)Field limiting
+    //127.0.0.1:3000/api/v1/tours?fields=name,duration,difficulty,price
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+    //4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+    //127.0.0.1:3000/api/v1/tours?pag2&limit=10
+
+    //EXECUTE QUERY
+    const tours = await query;
     //how to query
     /*   const query= Tour.find()
       .where('duration')
       .equals(5)
       .where('difficulty')
       .equals('easy'); */
-    //EXECUTE QUERY
-    const tours = await query;
-
     //SEND RESPONSE
     res.status(200).json({
       status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,11 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-/* router.param('id', tourController.checkID); */
+
+//127.0.0.1:3000/api/v1/tours/top-5-cheap
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 router
   .route('/')
   .get(tourController.getAllTours)
