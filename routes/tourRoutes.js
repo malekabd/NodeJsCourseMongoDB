affadc7f82183c98a8b09bfd8407549e96d17125
malekabd <61131777+malekabd@users.noreply.github.com>
2023-09-03 20:04:59 +0300

    104

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,11 +3,13 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-
 //127.0.0.1:3000/api/v1/tours/top-5-cheap
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
   .get(tourController.getAllTours)
