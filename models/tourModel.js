eaf8e3578b26f6846aad90fc5caaa2ddda0dce57
malekabd <61131777+malekabd@users.noreply.github.com>
2023-09-18 18:30:51 +0300

    106

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -7,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug:String,
     duration: { type: Number, required: [true, 'A tour must have a duration'] },
     maxGroupSize: {
       type: Number,
@@ -50,7 +53,15 @@ tourSchema.virtual('durationWeeks').get(function () {
   //this can be used only with normal function
   return this.duration / 7;
 });
-
+//Document Middleware rns before the .save() and the .create()
+tourSchema.pre('save',function (next){
+  this.slug = slugify(this.name,{lower:true})
+  next();
+})
+tourSchema.post('save',function(doc,next){
+  console.log(doc);
+  next()
+})
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
