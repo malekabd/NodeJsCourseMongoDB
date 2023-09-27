95ff58d26538f414c1281ddd168946d547ca4df6
malekabd <61131777+malekabd@users.noreply.github.com>
2023-09-27 12:29:15 +0300

    137 done

--- models/userModal.js
+++ models/userModal.js
@@ -50,6 +50,12 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000; //to make sure that the token is created after the password has been changed
+  next();
+});
+
 userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
