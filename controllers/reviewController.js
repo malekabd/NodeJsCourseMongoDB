c242e0df90eb45e763e3bd1210bf87284a22b761
malekabd <61131777+malekabd@users.noreply.github.com>
2023-10-01 12:22:47 +0300

    158 not done

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,6 +14,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 exports.createReview = catchAsync(async (req, res, next) => {
+  //Allow nested routes
+  if (!req.body.tour) req.params.tour = req.params.tourId;
+  if (!req.body.user) req.params.user = req.user.id; //the id comes from middleware protection
+
   const newReview = await Review.create(req.body);
   res.status(201).json({
     status: 'success',
