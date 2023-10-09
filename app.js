8bce0bd50974f69d407eb02221ce4dd8e4fd6e17
malekabd <61131777+malekabd@users.noreply.github.com>
2023-10-09 18:06:05 +0300

    181 done

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRoute = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -66,15 +67,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.get('/', (req, res) => {
-  res.status(200).render('base', { tour: 'The Forest Hiker', user: 'Malek' });
-});
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-});
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRoute);
